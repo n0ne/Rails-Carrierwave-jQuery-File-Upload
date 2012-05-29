2950410df57939aa1f6c43494830c4d378dc4760
none <alexander.podkidyshev@gmail.com>
2012-05-29 10:41:02 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GalleryJqueryFileUpload::Application.config.secret_token = '218bc30d20549293447465f84c339eb785da0118ab5873a6b94f09a3d64074a054d1ec2791d06a949abc7e625a17c147b4ba5bcb7e2613fea59429c01bc6072d'
